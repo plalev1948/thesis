fd0f0bce7fea7112e9b42142b2d6a6798f5ea2e5
plalev1948 <pavellalev@abv.bg>
2023-07-25 20:57:39 +0300

    fix error of loading and displaying image

--- src/ship.py
+++ src/ship.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 
 class Ship:
@@ -5,16 +6,20 @@ class Ship:
     """Initialize the ship and set its starting position."""
 
     def __init__(self, ai_game):
-      self.screen = ai_game.screen
-      self.screen_rect = ai_game.screen.get_rect()
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
 
-      # Load the ship image and get its rect.
-      self.image = pygame.image.load('images/ship.bmp')
-      self.rect = self.image.get_rect()
+        # Get the directory path of the current script file.
+        current_dir = os.path.dirname(os.path.abspath(__file__))
 
-      # Start each new ship at the bottom center of the screen.
-      self.rect.midbottom = self.screen_rect.midbottom
+        # Construct the path to the ship image.
+        image_path = os.path.join(current_dir, 'images', 'ship.bmp')
+        self.image = pygame.image.load(image_path)
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
 
     def blitme(self):
-       """Draw the ship at its current location."""
-       self.screen.blit(self.image, self.rect)
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
