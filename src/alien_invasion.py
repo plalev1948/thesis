f66a6a02e30701ae9458a5a082170dcf63179c4f
plalev1948 <pavellalev@abv.bg>
2023-07-30 23:22:36 +0300

    shooting aliens: detecting bullet collisions

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -42,6 +42,10 @@ class AlienInvasion:
         # Update bullet positions.
         self.bullets.update()
 
+        # Check for any bullets that have hit aliens.
+        # if so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update the positions of all aliens in the fleet."""
         self._check_fleet_edges()
