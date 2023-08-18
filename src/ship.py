6a973f2f72ff8863167b0a1606dd27df7ed488c8
plalev1948 <pavellalev@abv.bg>
2023-08-19 01:26:45 +0300

    add: display the number of remaining lives

--- src/ship.py
+++ src/ship.py
@@ -1,11 +1,12 @@
 import os
 import pygame
+from pygame.sprite import Sprite
 
-class Ship:
+class Ship(Sprite):
     """A class to manage the ship."""
-    """Initialize the ship and set its starting position."""
 
     def __init__(self, ai_game):
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
